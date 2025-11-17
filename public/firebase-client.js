611a9ac8543df3bacfd6a606c9a2939b03e28d2a
cricketvs <94782748+webvss@users.noreply.github.com>
2025-11-17 11:17:44 +0530

    Create firebase-client.js

---
+++ public/firebase-client.js
@@ -0,0 +1,19 @@
+<!-- Firebase Web SDK -->
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js"></script>
+
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyDSDw6pDHxBR8dNdCL5hpHfWxYaC9ZtIZg",
+    authDomain: "cricketvs-5e595.firebaseapp.com",
+    projectId: "cricketvs-5e595",
+    storageBucket: "cricketvs-5e595.appspot.com",
+    messagingSenderId: "947578850537",
+    appId: "1:947578850537:web:67d84cf056fb50b1c3f478",
+    measurementId: "G-S71DWGH252"
+  };
+
+  // Init Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
